42a7ea3bc270bebd1a60a023fcb306e9a93bf7bb
Margosha <meow.margosha@gmail.com>
2023-10-24 21:59:52 +0300

    hw 2

---
+++ hw2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное
+// число и на выходе показывает вторую цифру этого числа
+Console.WriteLine("Enter a three digits number:");
+int num = int.Parse(Console.ReadLine());
+if (num <= 99 || num >= 1000) {
+    Console.WriteLine("Incorrect number {0}.", num);
+    return;
+}
+int second_digit = (num / 10) % 10;
+Console.WriteLine("The second digit is {0}.", second_digit);
